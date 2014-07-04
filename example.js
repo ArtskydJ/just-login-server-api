0382d59706caa1b62b9628a0a3e14bfccc6e427e
Joseph Dykstra <josephdykstra@gmail.com>
2014-07-03 19:46:38 -0500

    Fixed the server api

--- example.js
+++
@@ -1,5 +0,0 @@
-var Jlsa = require('just-login-server-api')
-var Jlc = require('just-login-core')
-var level = require('level-mem')
-var db = level('uniqueNameHere')
-var jlc = Jlc(db)
--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-var reqList = require('./server.js')
-var api = require('./api.js')
+function getFullApi(jlc, sessionId) {
+	return {
+		beginAuthentication: jlc.beginAuthentication.bind(jlc, sessionId),
+		isAuthenticated: jlc.isAuthenticated.bind(jlc, sessionId),
+		unauthenticate: jlc.unauthenticate.bind(jlc, sessionId)
+	}
+}
 
-module.exports = {
-	requestListener: reqList,
-	api: api
+function UUID() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
+		return v.toString(16)
+	})
+}
+
+function createNewSession(jlc, cb) { //cb(err, api, sessionId)
+	var sessionId = UUID()
+	cb(null, getFullApi(jlc, sessionId), sessionId)
+}
+
+function continueExistingSession(jlc, sessionId, cb) { //cb(err, api, sessionId)
+	jlc.isAuthenticated(sessionId, function(err, addr) {
+		if (err)
+			cb(err)
+		else if (!addr) {
+			var temp = new Error("Invalid Session Id")
+			temp.invalidSessionId = true
+			cb(temp)
+		} else
+			cb(null, getFullApi(jlc, sessionId), sessionId)
+	})
+}
+
+module.exports = function(justLoginCore) { //Exposed to the browser via dnode
+	return {
+		createNewSession: createNewSession.bind(null, justLoginCore),
+		continueExistingSession: continueExistingSession.bind(null, justLoginCore)
+	}
 }
