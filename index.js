23e74fabcb4118555c0cffe1af44820c2a08a20f
Joseph Dykstra <josephdykstra@gmail.com>
2014-08-09 14:19:21 -0500

    cleanup deleted adding properties to Error object added a name to an anonymous object for stack traces

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function getFullApi(jlc, sessionId) {
 }
 
 function UUID() {
-	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
 		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
 		return v.toString(16)
 	})
@@ -19,22 +19,18 @@ function createNewSession(jlc, cb) { //cb(err, api, sessionId)
 }
 
 function continueExistingSession(jlc, sessionId, cb) { //cb(err, api, sessionId)
-	jlc.isAuthenticated(sessionId, function(err, addr) {
+	jlc.isAuthenticated(sessionId, function (err, addr) {
 		if (err) {
-			err.ces = true
 			cb(err)
 		} else if (!addr) {
-			var temp = new Error("Invalid Session Id")
-			temp.invalidSessionId = true
-			temp.ces = true
-			cb(temp)
+			cb(new Error("Invalid Session Id"))
 		} else {
 			cb(null, getFullApi(jlc, sessionId), sessionId)
 		}
 	})
 }
 
-module.exports = function(justLoginCore) { //Exposed to the browser via dnode
+module.exports = function Jlsa(justLoginCore) { //Exposed to the browser via dnode
 	return {
 		createNewSession: createNewSession.bind(null, justLoginCore),
 		continueExistingSession: continueExistingSession.bind(null, justLoginCore)
